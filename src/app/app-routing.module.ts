94c7fcb312484ef65fb45cb8b980e10331b266fc
Ivan Moll Moll <imollm@uoc.edu>
2021-11-12 01:12:33 +0100

    CIDH-3 Add Media Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'administration', loadChildren: () => import('./administration/administration.module').then(m => m .AdministrationModule) },
   { path: 'event', loadChildren: () => import('./event/event.module').then(m => m.EventModule) },
+  { path: 'media', loadChildren: () => import('./media/media.module').then(m => m.MediaModule) },
 ];
 
 @NgModule({
