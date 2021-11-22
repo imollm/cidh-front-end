ef415f213fffd9709893ec2e0bc324f069c23872
Ivan Moll Moll <imollm@uoc.edu>
2021-11-22 15:55:46 +0100

    CIDH-69 Add signup route

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './components/auth/signup/signup.component';
 import { ProfileComponent } from './profile.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
