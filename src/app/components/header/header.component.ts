2574fb6e32f8df919115346990b8c5402db6e68e
Ivan Moll <imollm@uoc.edu>
2021-11-22 12:36:22 +0100

    CIDH-61 Implement open/close mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -9,14 +9,21 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
 
   faMobileMenuIcon = faBars;
+  @ViewChild('MobileMenu') mobileMenu: ElementRef;
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+    this.mobileMenu = this.element;
+  }
 
   ngOnInit(): void {
   }
 
   closeMobileMenu(): void {
+    this.mobileMenu.nativeElement.style.display = 'none';
+  }
 
+  openMobileMenu(): void {
+    this.mobileMenu.nativeElement.style.display = 'block';
   }
 
 }
