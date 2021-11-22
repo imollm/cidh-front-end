be352eca10e4716948fa35160ddf4bf87c1d22c8
Ivan Moll Moll <imollm@uoc.edu>
2021-11-22 15:54:15 +0100

    CIDH-69 Create SignUp component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
+    SignupComponent,
   ],
   imports: [
     CommonModule,
