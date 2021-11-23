b73d32a0e1d375480366bac8d617ff25ea86c392
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 09:58:46 +0100

    CIDH-68 Import ReactiveFormsModule

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
@@ -13,7 +14,8 @@ import { LoginComponent } from './components/auth/login/login.component';
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ProfileModule { }
