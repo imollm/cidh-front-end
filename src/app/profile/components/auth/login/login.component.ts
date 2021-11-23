072ab4167d78670bd6622e926d272bc3eb8bec5c
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 09:54:44 +0100

    CIDH-68 Implement login form

--- src/app/profile/components/auth/login/login.component.ts
+++ src/app/profile/components/auth/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formTitle = 'Inicia sessió';
+
   constructor() { }
 
   ngOnInit(): void {
