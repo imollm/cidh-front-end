4b149750412d4f3e24941e11e3482fb48271f7d0
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 09:53:51 +0100

    CIDH-68 Add login route

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 import { ProfileComponent } from './profile.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
