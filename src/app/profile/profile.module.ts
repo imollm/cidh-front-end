ebc6d6f7d3217f7ed268daa8c64c0d902d4bf7f4
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 09:32:26 +0100

    CIDH-68 Create login component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
