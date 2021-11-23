7bccb68ec36f031b1a518a48c112966e9a2397e8
Ivan Moll Moll <imollm@uoc.edu>
2021-11-24 00:12:08 +0100

    CIDH-73 Create an array of events
    Loop over array and render it through card component

--- src/app/components/upcoming/upcoming.component.ts
+++ src/app/components/upcoming/upcoming.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../../event/models/event';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-upcoming',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpcomingComponent implements OnInit {
 
-  constructor() { }
+  upcomingEvents: Event[] = [];
+
+  constructor() {
+    this.initEvents();
+  }
 
   ngOnInit(): void {
+  }
+
+  private initEvents(): void {
+    let event = {} as Event;
+    for (let i = 0; i < 4; i++) {
+      event.name = faker.name.title();
+      event.description = faker.commerce.productDescription();
+      event.picture = faker.image.imageUrl();
+      event.rating = faker.datatype.number();
+      event.location = faker.internet.url();
+      event.initDate = faker.datatype.datetime();
+      event.endDate = faker.datatype.datetime();
+      this.upcomingEvents.push(event);
+      event = {} as Event;
+    }
   }
 
 }
