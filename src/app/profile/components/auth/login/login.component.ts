1dc7c40cad2b1cd1b7cc4de373ab24180875ddcf
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 09:59:10 +0100

    CIDH-68 Implement form with Angular reactive forms

--- src/app/profile/components/auth/login/login.component.ts
+++ src/app/profile/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   formTitle = 'Inicia sessió';
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
   }
 
 }
