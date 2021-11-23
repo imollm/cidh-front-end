e6628c218642a8f63a5fa686168d8d0be512ab5e
Ivan Moll Moll <imollm@uoc.edu>
2021-11-23 10:42:12 +0100

    CIDH-73 Create banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
