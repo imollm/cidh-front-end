b6bd2db668e38f633cdb3c56d82585b9a2a46051
Ivan Moll Moll <imollm@uoc.edu>
2021-11-25 00:08:12 +0100

    CIDH-71 Add Swal modals to create a new question

--- src/app/media/components/forum/forum.component.ts
+++ src/app/media/components/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { Question } from '../../models/question';
 import { ForumService } from '../../services/forum.service';
 
@@ -11,11 +12,47 @@ export class ForumComponent implements OnInit {
 
   lastQuestions: Question[] = [];
   actualPage: number = 1;
+  newQuestion = {} as Question;
 
   constructor(private forumService: ForumService) { }
 
   ngOnInit(): void {
     this.lastQuestions = this.forumService.listAllQuestions('0');
+  }
+
+  addQuestion(): void {
+    Swal.fire({
+      title: 'Selecciona un event',
+      input: 'select',
+      inputOptions: {
+        event1: 'Event 1',
+        event2: 'Event 2',
+        event3: 'Event 3',
+      },
+      inputPlaceholder: 'Selecciona un event',
+      showCancelButton: true,
+      inputValidator: (eventId) => {
+        return new Promise((resolve) => {
+          this.newQuestion.eventId = eventId;
+          resolve(null)
+        })
+      }
+    }).then(() => {
+      Swal.fire({
+        title: 'Escriu la teva pregunta',
+        input: 'textarea',
+        inputPlaceholder: 'Quina pregunta tens...',
+        showCancelButton: true,
+        inputValidator: (message) => {
+          return new Promise((resolve) => {
+            this.newQuestion.message = message;
+            resolve(null)
+          })
+        }
+      })
+    }).then(() => {
+      console.log(this.newQuestion)
+    })
   }
 
 }
