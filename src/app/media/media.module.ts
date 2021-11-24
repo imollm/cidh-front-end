b95782bf133e2866ba53b6d27c5c0bc38a39a2cb
Ivan Moll Moll <imollm@uoc.edu>
2021-11-24 22:36:46 +0100

    CIDH-71 Add ngx-paginator dependency

--- src/app/media/media.module.ts
+++ src/app/media/media.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MediaRoutingModule } from './media-routing.module';
+
+// Custom
 import { MediaComponent } from './media.component';
 import { ForumComponent } from './components/forum/forum.component';
-
 import { SharedModule } from '../shared/shared.module';
 import { QuestionComponent } from './components/question/question.component';
 import { AnswerComponent } from './components/answer/answer.component';
+
+// Third party
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { AnswerComponent } from './components/answer/answer.component';
     CommonModule,
     MediaRoutingModule,
     SharedModule,
+    NgxPaginationModule,
   ]
 })
 export class MediaModule { }
