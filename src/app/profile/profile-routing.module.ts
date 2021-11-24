4f85da05a324deaf0488239d6fd415d517421d26
Ivan Moll Moll <imollm@uoc.edu>
2021-11-25 00:44:42 +0100

    CIDH-63 Add AuthGuard on dashboard route
    The purpose is if the user is not logged, he/she can't render dashboard

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './profile.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
