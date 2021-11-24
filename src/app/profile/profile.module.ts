bdbaa24d2418c4a34599437c4b7623eefcea50ad
Ivan Moll Moll <imollm@uoc.edu>
2021-11-25 00:37:12 +0100

    CIDH-63 Create dashboard component and his route

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,12 +6,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     ProfileComponent,
     LoginComponent,
     SignupComponent,
+    DashboardComponent,
   ],
   imports: [
     CommonModule,
