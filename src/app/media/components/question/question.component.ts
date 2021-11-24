23fbe74a4a67ac6f7be49b1951423fe358334b50
Ivan Moll Moll <imollm@uoc.edu>
2021-11-24 15:12:31 +0100

    CIDH-71 Add user icon and change layout of grid

--- src/app/media/components/question/question.component.ts
+++ src/app/media/components/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-question',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.sass']
 })
 export class QuestionComponent implements OnInit {
+
+  faUser = faUser;
 
   constructor() { }
 
