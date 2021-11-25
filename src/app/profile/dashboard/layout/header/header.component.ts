26facaaf2840d76bad53712f429f8b813c2f08a5
Ivan Moll Moll <imollm@uoc.edu>
2021-11-25 12:52:33 +0100

    CIDH-63 Implement dashboard header

--- src/app/profile/dashboard/layout/header/header.component.ts
+++ src/app/profile/dashboard/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard-header',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  faUserCircle = faUserCircle;
+  @ViewChild('profileMenu') profileMenu: ElementRef;
+
+  constructor(private elementRef: ElementRef) {
+    this.profileMenu = this.elementRef;
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleProfileMenu(): void {
+    this.profileMenu.nativeElement.style.display
+      = this.profileMenu.nativeElement.style.display
+        === 'none' ? 'block' : 'none';
   }
 
 }
