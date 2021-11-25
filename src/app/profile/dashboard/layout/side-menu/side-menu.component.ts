b7d2c9481311a7f2f43348cc84862f05151a413f
Ivan Moll Moll <imollm@uoc.edu>
2021-11-25 11:15:35 +0100

    CIDH-63 Change html tag name of header & side menu

--- src/app/profile/dashboard/layout/side-menu/side-menu.component.ts
+++ src/app/profile/dashboard/layout/side-menu/side-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-side-menu',
+  selector: 'app-dashboard-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.sass']
 })
