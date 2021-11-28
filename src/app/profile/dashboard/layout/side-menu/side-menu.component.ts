0050c0f9c2ec3c5be789b8325230815518663591
Ivan Moll Moll <imollm@uoc.edu>
2021-11-28 18:56:44 +0100

    CIDH-63 Fix open & close and responsive styles

--- src/app/profile/dashboard/layout/side-menu/side-menu.component.ts
+++ src/app/profile/dashboard/layout/side-menu/side-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, Input } from '@angular/core';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { Component, ElementRef, OnInit, ViewChild, Input, ViewChildren, QueryList } from '@angular/core';
+import { faAngleLeft, faAngleRight, faBriefcase, faCompressAlt, faCubes, faTags, faUserTie, faQuestion, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
 import { User } from 'src/app/profile/models/user';
 
 @Component({
@@ -7,21 +7,31 @@ import { User } from 'src/app/profile/models/user';
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.sass']
 })
-export class SideMenuComponent implements OnInit, AfterViewInit {
+export class SideMenuComponent implements OnInit {
 
-  @Input() user: User | undefined;
+  @Input() user: User | undefined;
 
   faArrowClose = faAngleLeft;
   faArrowOpen = faAngleRight;
 
-  @ViewChild('sidebarMenu') sidebarMenu: ElementRef;
-  @ViewChild('sidebarMenuTitle') sidebarMenuTitle: ElementRef;
+  faUserAdmin = faUserTie;
+  faCategories = faCubes;
+  faEventOrganizers = faBriefcase;
+  faAssociateAdminToEvent = faCompressAlt;
+  faLabels = faTags;
+  faQuestion = faQuestion;
+  faEvents = faCalendarDay;
+
+
+  @ViewChild('sideMenu') sideMenu: ElementRef;
+  @ViewChild('sideMenuTitle') sideMenuTitle: ElementRef;
   @ViewChild('closeSideMenuButton') closeSideMenuButton: ElementRef;
   @ViewChild('openSideMenuButton') openSideMenuButton: ElementRef;
+  @ViewChildren('sideMenuItemTitle') sideMenuItemsTitle!: QueryList<ElementRef>;
 
   constructor(private er: ElementRef) {
-    this.sidebarMenu = this.er;
-    this.sidebarMenuTitle = this.er;
+    this.sideMenu = this.er;
+    this.sideMenuTitle = this.er;
     this.closeSideMenuButton = this.er;
     this.openSideMenuButton = this.er;
   }
@@ -31,75 +41,87 @@ export class SideMenuComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     if (window.innerWidth <= 768) {
-      this.sidebarMenu.nativeElement.classList.add('closed');
       this.closeSideMenuButton.nativeElement.classList.add('hide');
       this.openSideMenuButton.nativeElement.classList.add('show');
     } else {
-      this.sidebarMenu.nativeElement.classList.add('opened');
       this.closeSideMenuButton.nativeElement.classList.add('show');
       this.openSideMenuButton.nativeElement.classList.add('hide');
     }
   }
 
   closeSideMenu(): void {
-    if (this.sidebarMenu.nativeElement.classList.contains('opened')) {
-      if (
-        this.closeSideMenuButton.nativeElement.classList.contains('show') &&
-        this.openSideMenuButton.nativeElement.classList.contains('hide')
-        ) {
-        // Toggle sidebar menu
-        this.sidebarMenu.nativeElement.classList.remove('opened');
-        this.sidebarMenu.nativeElement.classList.add('closed');
-        // Hide close button
-        this.closeSideMenuButton.nativeElement.classList.remove('show');
-        this.closeSideMenuButton.nativeElement.classList.add('hide');
-        // Show open button
-        this.openSideMenuButton.nativeElement.classList.remove('hide');
-        this.openSideMenuButton.nativeElement.classList.add('show');
-        // Show title
-        this.sidebarMenuTitle.nativeElement.classList.remove('show');
-        this.sidebarMenuTitle.nativeElement.classList.add('hide');
-      }
+    // Toggle sidebar menu
+    if (this.sideMenu.nativeElement.classList.contains('opened')) {
+      this.sideMenu.nativeElement.classList.remove('opened');
     }
+    this.sideMenu.nativeElement.classList.add('closed');
+    // Hide close button
+    if (this.closeSideMenuButton.nativeElement.classList.contains('show')) {
+      this.closeSideMenuButton.nativeElement.classList.remove('show');
+    }
+    this.closeSideMenuButton.nativeElement.classList.add('hide');
+    // Show open button
+    if (this.openSideMenuButton.nativeElement.classList.contains('hide')) {
+      this.openSideMenuButton.nativeElement.classList.remove('hide');
+    }
+    this.openSideMenuButton.nativeElement.classList.add('show');
+    // Show title
+    if (this.sideMenuTitle.nativeElement.classList.contains('show')) {
+      this.sideMenuTitle.nativeElement.classList.remove('show');
+    }
+    this.sideMenuTitle.nativeElement.classList.add('hide');
+    // Hide titles of items
+    this.sideMenuItemsTitle.forEach(element => {
+      (element.nativeElement as HTMLElement).style.display = 'none';
+    });
   }
 
   openSideMenu(): void {
-    if (this.sidebarMenu.nativeElement.classList.contains('closed')) {
-      if (
-        this.openSideMenuButton.nativeElement.classList.contains('show') &&
-        this.closeSideMenuButton.nativeElement.classList.contains('hide')
-        ) {
-        // Toggle sidebar menu
-        this.sidebarMenu.nativeElement.classList.remove('closed');
-        this.sidebarMenu.nativeElement.classList.add('opened');
-        // Hide open button
-        this.openSideMenuButton.nativeElement.classList.remove('show');
-        this.openSideMenuButton.nativeElement.classList.add('hide');
-        // Show close button
-        this.closeSideMenuButton.nativeElement.classList.remove('hide');
-        this.closeSideMenuButton.nativeElement.classList.add('show');
-        // Hide title
-        this.sidebarMenuTitle.nativeElement.classList.remove('hide');
-        this.sidebarMenuTitle.nativeElement.classList.add('show');
-      }
+    // Toggle side menu
+    if (this.sideMenu.nativeElement.classList.contains('closed')) {
+      this.sideMenu.nativeElement.classList.remove('closed');
     }
+    this.sideMenu.nativeElement.classList.add('opened');
+    // Hide open button
+    if (this.openSideMenuButton.nativeElement.classList.contains('show')) {
+      this.openSideMenuButton.nativeElement.classList.remove('show');
+    }
+    this.openSideMenuButton.nativeElement.classList.add('hide');
+    // Show close button
+    if (this.closeSideMenuButton.nativeElement.classList.contains('hide')) {
+      this.closeSideMenuButton.nativeElement.classList.remove('hide');
+    }
+    this.closeSideMenuButton.nativeElement.classList.add('show');
+    // Hide title
+    if (this.sideMenuTitle.nativeElement.classList.contains('hide')) {
+      this.sideMenuTitle.nativeElement.classList.remove('hide');
+    }
+    this.sideMenuTitle.nativeElement.classList.add('show');
+    // Show titles of items
+    this.sideMenuItemsTitle.forEach(element => {
+      let item = ((element.nativeElement as HTMLElement).parentNode as HTMLElement);
+      let icon = (item?.childNodes[0] as HTMLElement);
+      let title = (item?.childNodes[1] as HTMLElement);
+
+      item.style.justifyContent = 'start';
+      item.style.listStyle = 'none';
+      item.style.textAlign = 'left';
+      item.style.padding = '1em 0';
+      item.style.cursor = 'pointer';
+      item.style.paddingLeft = '1em';
+
+      icon.style.marginLeft = '1em';
+      icon.style.marginRight = '1em';
+      icon.style.fontSize = '24px';
+
+      title.style.display = 'inline-block';
+      title.style.fontSize = '1em';
+      title.style.fontWeight = 'bold';
+      title.style.color = '#161925';
+    });
   }
 
-  showSubMenu(event: Event): void {
-    const item = (event.target as HTMLElement);
-    const subMenu = item.querySelector('ul');
-    if (subMenu?.classList.contains('hide')) {
-      subMenu.classList.remove('hide');
-    }
-    subMenu?.classList.add('show');
-  }
+  hideSideMenuTitles(): void {
 
-  hideSubMenu(event: Event): void {
-    const item = (event.target as HTMLElement);
-    const subMenu = item.querySelector('ul');
-    if (subMenu?.classList.contains('show')) {
-      subMenu.classList.remove('show');
-    }
-    subMenu?.classList.add('hide');
   }
 }
