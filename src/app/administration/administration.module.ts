043658ee2fc163b9b436a9989125caf57aec082a
Ivan Moll Moll <imollm@uoc.edu>
2021-12-02 00:32:24 +0100

    CIDH-91 Create link admin to event component

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { AdministrationComponent } from './administration.component';
+import { LinkAdminToEventComponent } from './components/link-admin-to-event/link-admin-to-event.component';
 
 
 @NgModule({
   declarations: [
-    AdministrationComponent
+    AdministrationComponent,
+    LinkAdminToEventComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/administration/components/link-admin-to-event/link-admin-to-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-link-admin-to-event',
+  templateUrl: './link-admin-to-event.component.html',
+  styleUrls: ['./link-admin-to-event.component.sass']
+})
+export class LinkAdminToEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
