70122838e5e12ab849cd8eacbd17c90abadd4c0f
34653666003 <jorgegomila@gmail.com>
2021-12-01 20:38:44 +0100

    CIDH-83 Create user profile interface

---
+++ src/app/profile/models/userprofile.ts
@@ -0,0 +1,10 @@
+
+export interface Userprofile {
+    name: string;
+    surname: string;
+    fiscalId: string;
+    address: string;
+    language: string;
+    email: string;
+    password: string;
+}
