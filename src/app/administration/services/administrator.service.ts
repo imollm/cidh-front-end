89a5eae944ebc5f19e7c6267188791fd517510cf
Ivan Moll Moll <imollm@uoc.edu>
2021-12-04 23:53:02 +0100

    CIDH-91 Add TODO

--- src/app/administration/services/administrator.service.ts
+++ src/app/administration/services/administrator.service.ts
@@ -11,18 +11,23 @@ export class AdministratorService implements IAdministratorService {
   }
 
   addAdministrator(email: string, password: string, name: string, surname: string): void {
+    TODO: 'Method not implemented.'
     throw new Error('Method not implemented.');
   }
   updateAdministrator(email: string, password: string, name: string, surname: string): void {
+    TODO: 'Method not implemented.'
     throw new Error('Method not implemented.');
   }
   showAdministrator(email: string): void {
+    TODO: 'Method not implemented.'
     throw new Error('Method not implemented.');
   }
   listAllAdministrators(): void {
+    TODO: 'Method not implemented.'
     throw new Error('Method not implemented.');
   }
   assignAdministratorToEventOrganizer(email: string, name: string): void {
+    TODO: 'Method not implemented.'
     throw new Error('Method not implemented.');
   }
 }
