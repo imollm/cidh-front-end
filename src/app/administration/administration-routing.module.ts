71fdb087f1b9c1844542e54e82c87a008bb94cb2
Ivan Moll Moll <imollm@uoc.edu>
2021-12-05 00:27:41 +0100

    CIDH-94 Fix link admin-eventOrg routes

--- src/app/administration/administration-routing.module.ts
+++ src/app/administration/administration-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministrationComponent } from './administration.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { DashboardComponent } from '../profile/components/dashboard/dashboard.component';
+import { LinkAdminToEventOrganizerComponent } from '../administration/components/link-admin-to-event/link-admin-to-event-organizer.component';
 
-const routes: Routes = [{ path: '', component: AdministrationComponent }];
+const routes: Routes = [
+  { path: '', component: AdministrationComponent },
+  { path: 'dashboard', component: DashboardComponent,
+    // canActivate: [AuthGuard],
+    children: [
+      { path: 'linkAdminToEventOrganizer', component: LinkAdminToEventOrganizerComponent, /* canActivate: [AuthGuard] */ },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
