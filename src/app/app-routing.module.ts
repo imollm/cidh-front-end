01ebce588f5a18808756ba8b87217e7fa210f01c
Ivan Moll Moll <imollm@uoc.edu>
2021-12-05 00:35:14 +0100

    CIDH-96 Add categories route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path:'home', component: HomeComponent },
+  { path: 'categories', component: CategoriesComponent },
   { path: 'administration', loadChildren: () => import('./administration/administration.module').then(m => m .AdministrationModule) },
   { path: 'event', loadChildren: () => import('./event/event.module').then(m => m.EventModule) },
   { path: 'media', loadChildren: () => import('./media/media.module').then(m => m.MediaModule) },
