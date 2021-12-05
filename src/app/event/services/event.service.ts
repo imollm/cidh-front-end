b9a2598792f39fbe2179d43152cb20d68e512b7c
Ivan Moll Moll <imollm@uoc.edu>
2021-12-05 13:03:45 +0100

    CIDH-95 Fix interface name

--- src/app/event/services/event.service.ts
+++ src/app/event/services/event.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IEventService } from './event';
+import { IEventService } from './event.interface';
 
 @Injectable({
   providedIn: 'root'
