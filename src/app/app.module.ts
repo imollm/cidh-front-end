5cb4a49525626961116d52c846a47280a9d5a5d4
Ivan Moll Moll <imollm@uoc.edu>
2021-12-11 19:11:37 +0100

    CIDH-13 Add interceptor as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -37,7 +37,13 @@ import { AuthInterceptorService } from './api/auth-interceptor.service';
     BrowserAnimationsModule,
     BrowserModule
   ],
-  providers: [EndPointMapper, AuthInterceptorService],
+  providers: [
+    EndPointMapper,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
