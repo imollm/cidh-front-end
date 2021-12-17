2775fe9268bf37b3a69b53197d101b469c57bfdd
Ivan Moll <imollm@uoc.edu>
2021-12-17 21:59:16 +0000

    CIDH-9 Implementing form

--- src/app/administration/components/event-organizer/event-organizer-create/event-organizer-create.component.ts
+++ src/app/administration/components/event-organizer/event-organizer-create/event-organizer-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { EventOrganizerService } from 'src/app/administration/services/event-organizer/event-organizer.service';
+import { ModalResultService } from 'src/app/helpers/modal.service';
 
 @Component({
   selector: 'app-event-organizer-create',
@@ -7,7 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventOrganizerCreateComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'Crear una nova empresa';
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private eventOrganizerService: EventOrganizerService,
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private modalResultService: ModalResultService
+  ) {
+    this.form = this.fb.group({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit(): void {
   }
