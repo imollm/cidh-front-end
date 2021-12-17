b3b90cf3a8bbd82f86ff500681e753cb4955717e
Ivan Moll <imollm@uoc.edu>
2021-12-17 21:01:05 +0000

    CIDH-9 Implement list component

--- src/app/administration/components/event-organizer/event-organizer-list/event-organizer-list.component.ts
+++ src/app/administration/components/event-organizer/event-organizer-list/event-organizer-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { EventOrganizer } from 'src/app/administration/models/event-organizer.model';
 import { EventOrganizerService } from 'src/app/administration/services/event-organizer/event-organizer.service';
+import { ModalResultService } from 'src/app/helpers/modal.service';
+import { IActionButtons } from 'src/app/shared/components/table/models/action-buttons.model';
+import { IDashboardTable } from 'src/app/shared/components/table/models/table.model';
 
 @Component({
   selector: 'app-event-organizer-list',
@@ -8,11 +13,46 @@ import { EventOrganizerService } from 'src/app/administration/services/event-org
 })
 export class EventOrganizerListComponent implements OnInit {
 
+  eventOrganizers: EventOrganizer[] = [];
+  dataTable: IDashboardTable = {} as IDashboardTable;
+  actionButtons: IActionButtons = {
+    active: true,
+    resource: '/administration/event-organizer',
+    actions: {
+      view: true,
+      edit: true,
+      delete: false
+    }
+  } as IActionButtons;
+
   constructor(
-    private eventOrganizerService: EventOrganizerService
+    private eventOrganizerService: EventOrganizerService,
+    private spinner: NgxSpinnerService,
+    private modalResultService: ModalResultService
   ) { }
 
   ngOnInit(): void {
+    this.getEventOrganizers();
+  }
+
+  getEventOrganizers(): void {
+    this.spinner.show();
+    this.eventOrganizerService.listAllEventOrganizers().then(res => {
+      if (res.length > 0) {
+        this.eventOrganizers = res;
+        this.setDataTable();
+      }
+      this.spinner.hide();
+    });
+  }
+
+  setDataTable(): void {
+    this.dataTable.title = "Empreses registrades";
+    this.dataTable.colsName = [
+      { colName: "name",  text: "Nom" },
+      { colName: "description",  text: "Descripció" }
+    ];
+    this.dataTable.data = this.eventOrganizers;
   }
 
 }
