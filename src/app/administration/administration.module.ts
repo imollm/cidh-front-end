44435adff70c7881539514ca78b8423157e18f31
Ivan Moll <imollm@uoc.edu>
2021-12-17 21:00:45 +0000

    CIDH-9 Add shared module to administration module

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -6,6 +6,7 @@ import { AdministrationRoutingModule } from './administration-routing.module';
 import { AdministrationComponent } from './administration.component';
 import { LinkAdminToEventOrganizerComponent } from './components/event-organizer/link-admin-to-event/link-admin-to-event-organizer.component';
 import { EventOrganizerListComponent } from './components/event-organizer/event-organizer-list/event-organizer-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { EventOrganizerListComponent } from './components/event-organizer/event-
   ],
   imports: [
     CommonModule,
-    AdministrationRoutingModule
+    AdministrationRoutingModule,
+    SharedModule
   ]
 })
 export class AdministrationModule { }
