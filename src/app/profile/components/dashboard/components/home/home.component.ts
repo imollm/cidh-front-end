ad6ab2760af60d5efa10ca8d2044dccfc98612a3
Jorge Gomila <jgomilaa@uoc.edu>
2021-12-18 14:08:28 +0100

    CIDH-87-Implement-dashboard-home-for-every-role-from-design

--- src/app/profile/components/dashboard/components/home/home.component.ts
+++ src/app/profile/components/dashboard/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TableComponent } from 'src/app/shared/components/table/table.component';
+import { IDashboardTable } from 'src/app/shared/components/table/models/table.model';
+import { faTimes, faEye, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { IActionButtons } from 'src/app/shared/components/table/models/action-buttons.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,34 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  constructor() { }
+  formTitle= 'Home';
+  dataTable: any;
+  //actionsButtons: any;
+  constructor() 
+  { 
+    
+  }
+
+  ngOnInit(): void {
+    this.setDataTable();
+  }
+
+  setDataTable(): void {
+    this.dataTable.title = 'Títol de la taula';
+    this.dataTable.colsName = [
+      { colName: 'name', text: 'Nom' },
+      { colName: 'email', text: 'Email' },
+
+    ];
+    this.dataTable.data = [
+      { name: 'Juan', email: 'correo@correo.com'},
+      { name: 'Juan', email: 'correo@correo.com'},
+      
+    ];
+    this.dataTable.inverse = false;
+  }
+  
 
 }
+
+
