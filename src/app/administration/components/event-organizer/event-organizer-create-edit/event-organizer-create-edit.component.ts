2f4fbdb70531689b3e7818784e2a196f26974135
Ivan Moll <imollm@uoc.edu>
2021-12-18 16:32:30 +0000

    CIDH-9 Work in progess

--- src/app/administration/components/event-organizer/event-organizer-create-edit/event-organizer-create-edit.component.ts
+++ src/app/administration/components/event-organizer/event-organizer-create-edit/event-organizer-create-edit.component.ts
@@ -1,9 +1,12 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { EventOrganizer } from 'src/app/administration/models/event-organizer.model';
 import { EventOrganizerService } from 'src/app/administration/services/event-organizer/event-organizer.service';
 import { ModalResultService } from 'src/app/helpers/modal.service';
+import { UtilsService } from 'src/app/helpers/utils.helper.service';
 import { IUser } from 'src/app/profile/models/user.model';
 import { AuthService } from 'src/app/profile/services/auth/auth.service';
 
@@ -17,6 +20,8 @@ export class EventOrganizerCreateEditComponent {
   title: string = 'Crear una nova empresa';
   form: FormGroup;
   meAsSuperAdmin: IUser;
+  mode: string;
+  eventOrganizer: EventOrganizer;
 
   constructor(
     private fb: FormBuilder,
@@ -34,9 +39,50 @@ export class EventOrganizerCreateEditComponent {
   }
 
   ngOnInit(): void {
+    let mode = UtilsService.getMode(this.router.url); 
+
+    if (!mode && mode === 'create') {
+      this.createMode()
+    } else if (!mode && mode === 'edit') {
+      this.editMode();
+    } else {
+      this.router.navigate(['/administration/dashboard/event-organizer/list']);
+    }
+  }
+
+  createMode(): void {
+    this.getUser();
+    this.form.reset();
+  }
+
+  editMode(): void {
+    this.eventOrganizer.id = UtilsService.getResourceIdFromURI(this.router.url);
+    this.eventOrganizer.id 
+      ? this.getEventOrganizer(this.eventOrganizer)
+      : this.router.navigate(['/administration/dashboard/event-organizer/list']);
+  }
+
+  getUser(): void {
     this.authService.getUser().then(res => {
       if (res) {
         this.form.get('admin').setValue(res.id);
+      }
+    });
+  }
+
+  
+  getEventOrganizer(eventOrganizer: EventOrganizer): void {
+    this.eventOrganizerService.showEventOrganizer(eventOrganizer.id).then(res => {
+      this.spinner.show();
+      if (res) {
+        this.eventOrganizer = res;
+
+        this.form.patchValue({
+          id: this.eventOrganizer.id,
+          name: this.eventOrganizer.name,
+          description: this.eventOrganizer.description,
+          admin: this.eventOrganizer.admin
+        });
       }
     });
   }
@@ -55,5 +101,4 @@ export class EventOrganizerCreateEditComponent {
       }).then(() => this.spinner.hide());
     }
   }
-
 }
--- src/app/administration/models/event-organizer.model.ts
+++ src/app/administration/models/event-organizer.model.ts
@@ -2,4 +2,5 @@ export interface EventOrganizer {
     id?: string;
     name: string;
     description: string;
+    admin: string;
 }
