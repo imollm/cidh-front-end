09accf4e9cf0a3ac9ef04fe5ddabf0e17de747aa
raullc92 <89393783+raullc92@users.noreply.github.com>
2021-12-18 12:40:31 +0100

    CIDH-116 Add and implement interface

--- src/app/profile/services/profile-service.service.ts
+++ src/app/profile/services/profile-service.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IUser } from '../models/user.model'
 import { EndPointMapper } from 'src/app/helpers/endpoint-mapper.helper.service';
+import { IProfileService } from './profile-service.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileService {
+export class ProfileService implements IProfileService{
 
   constructor(private httpClient: HttpClient, private endPointMapper: EndPointMapper) {
    
