56c9d3450380cd172400d343585454faac53a140
Ivan Moll <imollm@uoc.edu>
2021-12-19 21:52:30 +0000

    CIDH-87 Refactor dashboard home component

--- src/app/profile/components/dashboard/components/home/home.component.ts
+++ src/app/profile/components/dashboard/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { TableComponent } from 'src/app/shared/components/table/table.component';
 import { IDashboardTable } from 'src/app/shared/components/table/models/table.model';
-import { faTimes, faEye, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { IActionButtons } from 'src/app/shared/components/table/models/action-buttons.model';
+import { AuthService } from 'src/app/profile/services/auth/auth.service';
+import { IUser } from 'src/app/profile/models/user.model';
+import { UtilsService } from 'src/app/helpers/utils.helper.service';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -12,19 +13,66 @@ import { IActionButtons } from 'src/app/shared/components/table/models/action-bu
 export class HomeComponent {
 
   formTitle= 'Home';
+  me: IUser;
+  role: string;
   dataTable: IDashboardTable = {} as IDashboardTable;
   actionButtons: IActionButtons = {
     active: false
   } as IActionButtons;
 
-  constructor() 
+  constructor(private authService: AuthService) 
   { }
 
   ngOnInit(): void {
-    this.setDataTable();
+    this.authService.getUser().then(res => {
+      if (res && res.id) {
+        this.me = res;
+      }
+    }).then(() => {
+      this.role = UtilsService.getRoleFromAccessToken();
+
+      if (this.role === 'USER') {
+        this.setUserHome();
+      } else if (this.role === 'ADMIN') {
+        this.setAdminHome();
+      } else if (this.role === 'SUPERADMIN') {
+        this.setSuperAdminHome();
+      }
+
+      this.setDataTable();
+    });
+  }
+
+  setUserHome(): void {
+    /**
+     * TODO: USER: last 3 subscribed events + last next 3 events to be celebrated + total of subscribed events (count).
+     * 2 tables
+     * 1 card
+     */
+  }
+
+  setAdminHome(): void {
+  /**
+   * TODO: ADMIN: total events that this admin manage (count) + last 3 events that this admin manage + pending questions to be answered.
+   * 2 tables
+   * 1 card
+   */
+  }
+
+  setSuperAdminHome(): void {
+    /**
+     * TODO: SUPERADMIN: last 3 events in the system + last 3 event organizers in the system + last 3 labels in the system + last 3 admins in the system + last 3 categories in the system + pending questions to be answered.
+     * 5 tables
+     * 1 card
+     */
   }
 
   setDataTable(): void {
+    let title: string;
+    let columns: [] = [];
+    let data: [] = [];
+    
+    
     this.dataTable.title = 'Títol de la taula';
     this.dataTable.colsName = [
       { colName: 'name', text: 'Nom' },
