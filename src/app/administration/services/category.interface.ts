4523fe34db26759bb56566ee000cef26a5a7647d
raullc92 <89393783+raullc92@users.noreply.github.com>
2021-12-20 18:10:52 +0100

    CIDH-8 Modify category services and interface

--- src/app/administration/services/category.interface.ts
+++ src/app/administration/services/category.interface.ts
@@ -2,7 +2,7 @@ import { Category } from "../models/category.model";
 
 export interface ICategoryService {
   addCategory(category: Category): Promise<Category>;
-  updateCategory(category: Category): Promise<Category>;
-  showCategory(category: Category): Promise<Category>;
+  updateCategory(category: Category, categoryId: string): Promise<Category>;
+  showCategory(categoryId: string): Promise<Category>;
   listAllCategories(): Promise<Category[]>;
 }
