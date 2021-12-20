b1dbbdcf98139c3886784268fff1c0ede1266cc9
Jorge Gomila <jgomilaa@uoc.edu>
2021-12-20 21:30:18 +0100

    CIDH-104 Fix bug

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/profile/services/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.sass']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  
+  constructor(private authService: AuthService) { }
 
-  constructor() { }
+  ngOnInit(): void {
+    console.log(!this.isLogged());
+  }
 
+  isLogged(): Boolean{
+    return !this.authService.isLogged();
+  }
 }
