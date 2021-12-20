3d66d779b5b82e3dafd94b2477af2334fc97e498
Ivan Moll <imollm@uoc.edu>
2021-12-20 14:07:16 +0000

    CIDH-87 Add data model to represent home data of every role

--- src/app/profile/components/dashboard/components/home/home.component.ts
+++ src/app/profile/components/dashboard/components/home/home.component.ts
@@ -44,34 +44,21 @@ export class HomeComponent {
   }
 
   setUserHome(): void {
-    /**
-     * TODO: USER: last 3 subscribed events + last next 3 events to be celebrated + total of subscribed events (count).
-     * 2 tables
-     * 1 card
-     */
+     let userHomeData = this.me.homeData;
   }
 
   setAdminHome(): void {
-  /**
-   * TODO: ADMIN: total events that this admin manage (count) + last 3 events that this admin manage + pending questions to be answered.
-   * 2 tables
-   * 1 card
-   */
+   let adminHomeData = this.me.homeData;
   }
 
   setSuperAdminHome(): void {
-    /**
-     * TODO: SUPERADMIN: last 3 events in the system + last 3 event organizers in the system + last 3 labels in the system + last 3 admins in the system + last 3 categories in the system + pending questions to be answered.
-     * 5 tables
-     * 1 card
-     */
+    let superAdminHomeData = this.me.homeData;
   }
 
   setDataTable(): void {
     let title: string;
     let columns: [] = [];
     let data: [] = [];
-    
     
     this.dataTable.title = 'Títol de la taula';
     this.dataTable.colsName = [
--- src/app/profile/models/user.model.ts
+++ src/app/profile/models/user.model.ts
@@ -1,3 +1,52 @@
+/**
+ * TODO: USER: 
+ * last 3 subscribed events + 
+ * last next 3 events to be celebrated + 
+ * total of subscribed events (count).
+ * 
+ * 2 tables
+ * 1 card
+ */
+interface UserHomeData {
+    subscribedEvents: [];
+    eventsToBeCelebrated: [];
+    totalOfSubscribedEvents: number;
+}
+/**
+   * TODO: ADMIN:
+   * total events that this admin manage (count) + 
+   * last 3 events that this admin manage + 
+   * pending questions to be answered.
+   * 
+   * 2 tables
+   * 1 card
+   */
+interface AdminHomeData {
+    totalEventsManaged: number;
+    eventsManaged: [];
+    pendingQuestionsToBeAnswered: [];
+}
+/**
+     * TODO: SUPERADMIN: 
+     * last 3 events in the system + 
+     * last 3 event organizers in the system + 
+     * last 3 labels in the system + 
+     * last 3 admins in the system + 
+     * last 3 categories in the system + 
+     * pending questions to be answered.
+     * 
+     * 5 tables
+     * 1 card
+     */
+interface SuperAdminHomeData {
+    lastSavedEventsInTheSystem: [];
+    lastEventOrganizersInTheSystem: [];
+    lastLabelsInTheSystem: [];
+    lastAdminsInTheSystem: [];
+    lastCategoriesInTheSystem: [];
+    pendingQuestionsToBeAnswered: [];
+}
+
 export interface IUser {
     id?: string;
     firstName: string;
@@ -7,5 +56,5 @@ export interface IUser {
     preferredLanguage: string;
     email: string;
     password?: string;
-    role: string;
+    homeData?: UserHomeData | AdminHomeData | SuperAdminHomeData;
 }
