2cbed75637e6b4684b58170798e805dcf02c3ddb
Ivan Moll <imollm@uoc.edu>
2021-12-25 23:12:35 +0000

    Fix profile form

--- src/app/profile/components/dashboard/components/profile-show/profile-show.component.ts
+++ src/app/profile/components/dashboard/components/profile-show/profile-show.component.ts
@@ -5,6 +5,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ModalResultService } from 'src/app/helpers/modal.service';
 import { ProfileService } from '../../../../services/profile-service.service'
 import { preferredLanguages } from 'src/app/profile/models/preferredLanguages.model';
+import { UtilsService } from 'src/app/helpers/utils.helper.service';
 
 
 @Component({
@@ -33,6 +34,8 @@ export class ProfileShowComponent implements OnInit {
       address: new FormControl('', Validators.required),
       preferredLanguage: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.email, Validators.required]),
+      password1: new FormControl('', [UtilsService.strong, Validators.minLength(8)]),
+      password2: new FormControl('', [UtilsService.strong, Validators.minLength(8)])
     });
   }
 
@@ -70,5 +73,18 @@ export class ProfileShowComponent implements OnInit {
         });
       });
     }
+  }
+
+  isPwdEquals(): boolean {
+    return this.form.get('password1').value === this.form.get('password2').value;
+  }
+
+  setPassword(): void {
+    const pwd = this.form.value.password1;
+
+    delete this.form.value.password1;
+    delete this.form.value.password2;
+
+    this.form.value.password = pwd;
   }
 }
