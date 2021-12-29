884a9f390934ece63f1d205e6b7c98851f9b480f
Ivan Moll <imollm@uoc.edu>
2021-12-29 17:52:17 +0000

    CIDH-28 Change data models

--- src/app/event/models/event.model.ts
+++ src/app/event/models/event.model.ts
@@ -1,19 +1,18 @@
+import { Category } from "src/app/administration/models/category.model";
+import { IEventOrganizer } from "src/app/administration/models/event-organizer.model";
+import { Label } from "src/app/administration/models/label.model";
+import { IRating } from "./rating.model";
 export interface IEvent {
     id?: string;
     name: string;
-    description: string;
     headerImage?: string;
-    rating?: number;
-    location: string;
+    rating?: IRating;
+    description: string;
     startDate?: Date;
     endDate?: Date;
+    category: Category;
+    labels: Label[];
+    eventOrganizer: IEventOrganizer;
+    eventUrl: string;
     isFavorite?: boolean;
-    // category: Category;
-    // labels: Label[];
-    // eventOrganizer: EventOrganizer;
-    // administrator: User;
-    // questions: Question[];
-    // comments: Comment[];
-    // ratings: Rating[];
-    // subscribers: User[];
 }
