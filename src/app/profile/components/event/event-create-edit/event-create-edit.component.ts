4ba679a3a1e56bc2aadb9f84c3b9d1e6d36161ce
Ivan Moll <imollm@uoc.edu>
2021-12-31 16:22:02 +0000

    CIDH-15 - Fix FormArray on CREATE/EDIT mode - Fix Validators - Fix startDate/endDate fields

--- src/app/profile/components/event/event-create-edit/event-create-edit.component.ts
+++ src/app/profile/components/event/event-create-edit/event-create-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 import { IEvent } from 'src/app/event/models/event.model';
 import { Form, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,13 +13,14 @@ import { Category } from 'src/app/administration/models/category.model';
 import { CategoryService } from 'src/app/administration/services/category/category.service';
 import { Label } from 'src/app/administration/models/label.model';
 import { LabelService } from 'src/app/administration/services/label/label.service';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-event-create-edit',
     templateUrl: './event-create-edit.component.html',
     styleUrls: ['./event-create-edit.component.sass']
 })
-export class EventCreateEditComponent implements OnInit {
+export class EventCreateEditComponent implements OnInit, AfterViewChecked {
 
     title: string = 'Crea un nou acte'
     btnText: string = 'Crear'
@@ -31,6 +32,7 @@ export class EventCreateEditComponent implements OnInit {
     eventOrganizers: IEventOrganizer[] = []
     labels: Label[] = []
     selectedLabels = []
+    private readonly URLRegex: string = '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$';
 
 
     constructor(
@@ -52,64 +54,138 @@ export class EventCreateEditComponent implements OnInit {
             category: new FormControl('', Validators.required),
             organizerId: new FormControl('', Validators.required),
             labelIds: new FormArray([]),
-            eventUrl: new FormControl('', Validators.required)
+            eventUrl: new FormControl('', [Validators.required, Validators.pattern(this.URLRegex)])
         });
+
+    }
+
+    ngAfterViewChecked(): void {
+        if (this.labels.length > 0) { 
+            this.initLabelCheckboxes();
+        }
     }
 
     ngOnInit(): void {
         this.mode = UtilsService.getMode(this.router.url);
 
+        this.getLabels();
+        this.getCategories();
+        this.getEventOrganizers();
+
         if (this.mode && this.mode === 'create') {
             this.createMode();
         } else if (this.mode && this.mode === 'edit') {
             this.editMode();
-            this.title = 'Crea un nou acte';
+            this.title = 'Edita l\'acte';
             this.btnText = 'Edita';
         }
-
-        this.getCategories()
-        this.getEventOrganizers()
-        this.getLabels()
     }
 
-    createMode(): void {
+    private async getLabels(): Promise<void> {
+        this.labels = await this.labelService.listAllLabels();
+    }
+
+    private async getCategories(): Promise<void> {
+        this.categories = await this.categoryService.listAllCategories();
+    }
+
+    private async getEventOrganizers(): Promise<void> {
+        this.eventOrganizers = await this.eventOrganizerService.listAllEventOrganizers();
+    }
+
+    private initLabelCheckboxes(): void {
+        if (this.mode === 'create') {
+            this.labels.forEach(() => this.labelIdsFormArray.push(new FormControl(false)));
+
+        } else if (this.mode === 'edit') {
+            let isSelected = false;
+
+            if (this.labels && this.labels.length > 0 && this.event.labels) {
+                this.labels.forEach(label => {
+                    isSelected = false;
+    
+                    this.event.labels.forEach(eventLabel => {
+                        if (eventLabel.id === label.id) {
+                            isSelected = true;
+                        }
+                    })
+    
+                    this.labelIdsFormArray.push(new FormControl(isSelected))
+                })
+            }
+        }
+    }
+
+    private createMode(): void {
         this.form.reset();
     }
 
-    editMode(): void {
+    private editMode(): void {
         this.eventId = UtilsService.getResourceIdFromURI(this.router.url);
         this.eventId
             ? this.getEvent()
             : this.router.navigate(['/profile/dashboard/events/list']);
     }
 
+    get labelIdsFormArray(): FormArray {
+        return this.form.controls.labelIds as FormArray;
+    }
 
-    getEvent() {
-        this.eventService.showEvent(this.eventId).then(res => {
-            this.spinner.show();
+    private async getEvent(): Promise<void> {
+        this.spinner.show();
 
-            if (res) {
-                this.event = res;
-                this.eventId = this.event.id;
-                this.form.patchValue({
-                    name: this.event.name,
-                    description: this.event.description,
-                    headerImage: this.event.headerImage,
-                    eventUrl: this.event.eventUrl,
-                    category: this.event.category.id,
-                    organizerId: this.event.eventOrganizer.id
-                });
-            }
-            this.spinner.hide();
+        this.event = await this.eventService.showEvent(this.eventId);
+
+        this.eventId = this.event.id;
+
+        this.form.patchValue({
+            name: this.event.name,
+            description: this.event.description,
+            headerImage: this.event.headerImage,
+            eventUrl: this.event.eventUrl,
+            category: this.event.category.name,
+            organizerId: this.event.eventOrganizer.id,
+            startDate: new Date(this.event.startDate * 1000).toISOString().split('T')[0],
+            endDate: new Date(this.event.endDate * 1000).toISOString().split('T')[0]
         });
+
+        this.spinner.hide();
+    }
+
+    private prepareDatesToBeSendIt(): void {
+        this.form.value.startDate = this.form.value.startDate !== ''
+            ? new Date(this.form.get('startDate').value).getTime() / 1000
+            : null;
+        this.form.value.endDate = this.form.value.endDate !== ''
+            ? new Date(this.form.get('endDate').value).getTime() / 1000
+            : null;
+    }
+
+    private prepareLabelIdsToBeSendIt(): void {
+        this.form.value.labelIds =
+            this.form.value.labelIds
+                .map((checked, i) => checked && (i < this.labels.length) ? this.labels[i].id : null)
+                .filter(v => v !== null);
+    }
+
+    private redirectUser(): void {
+        this.router.navigate(['/profile/dashboard/events/list']).then(() => {
+            let result: Boolean = this.event.hasOwnProperty('id');
+
+            if (this.mode === 'create') {
+                this.modalResultService.createResultModal(result);
+            } else if (this.mode === 'edit') {
+                this.modalResultService.editResultModal(result);
+            }
+        }).then(() => this.spinner.hide());
     }
 
     onSubmit(): void {
         if (this.form.valid) {
             this.spinner.show();
 
-            this.form.value.startDate = new Date(this.form.get('startDate').value).getTime() / 1000;
-            this.form.value.endDate = new Date(this.form.get('endDate').value).getTime() / 1000;
+            this.prepareDatesToBeSendIt();
+            this.prepareLabelIdsToBeSendIt();
 
             if (this.mode && this.mode === 'create') {
                 this.eventService.addEvent(this.form.value).then(res => {
@@ -122,49 +198,5 @@ export class EventCreateEditComponent implements OnInit {
             }
         }
     }
-
-    redirectUser(): void {
-        this.router.navigate(['/profile/dashboard/events/list']).then(() => {
-            let result: Boolean = this.event.hasOwnProperty('id');
-
-            if (this.mode === 'create') {
-                this.modalResultService.createResultModal(result);
-            } else if (this.mode === 'edit') {
-                this.modalResultService.editResultModal(result);
-            }
-        }).then(() => this.spinner.hide());
-    }
-
-    getCategories(): void {
-        this.categoryService.listAllCategories().then(res => {
-            this.categories = res
-        })
-    }
-
-    getEventOrganizers(): void {
-        this.eventOrganizerService.listAllEventOrganizers().then(res => {
-            this.eventOrganizers = res
-        })
-    }
-
-    getLabels(): void {
-        this.labelService.listAllLabels().then(res => {
-            this.labels = res
-        })
-    }
-
-    get eventLabels(): FormArray {
-        return this.form.get('eventLabels') as FormArray
-    }
-
-    onChange(name: string, event) {
-        if (event.target.checked) {
-            this.selectedLabels.push(name)
-        } else {
-            const index = this.selectedLabels.find(label => label === name)
-            this.selectedLabels.splice(index)
-        }
-        this.form.get('labelIds').setValue(this.selectedLabels)
-        console.log(this.form.value)
-    }
 }
+
