4767df29392806169100b79d3bc0698b484894c2
Ivan Moll <imollm@uoc.edu>
2021-12-31 17:56:44 +0000

    CIDH-18 Fix title of banner

--- src/app/shared/components/banner/banner.component.ts
+++ src/app/shared/components/banner/banner.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.sass']
 })
-export class BannerComponent {
+export class BannerComponent implements OnChanges, AfterContentChecked {
 
-  bannerTitle = 'CULTURE IN DA HOUSE';
+  @Input() bannerTitle: string;
 
   constructor() { }
 
+  ngAfterContentChecked(): void {
+    
+  }  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.bannerTitle = changes.bannerTitle.currentValue;
+  }
 }
